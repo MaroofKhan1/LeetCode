fdc8eda31102496bea7b1630682280c90227d111
MaroofKhan1 <102704079+MaroofKhan1@users.noreply.github.com>
2022-09-12 23:22:46 -0500

    Time: 139 ms (51.78%), Space: 51.2 MB (33.98%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+function containsDuplicate(nums) {
+    //Create a memory o of 1 
+    //Go through array and see if what i seen now, is something i've seen before. 
+        //if my item is never seen before put it in memory
+        //if the number dose exist in memory, return true
+    
+    let memory = {};
+    for(let i = 0; i < nums.length; i++){
+        if(memory[nums[i]] == undefined){
+            memory[nums[i]] = "hello"
+        } else {
+            return true
+        }
+    }
+    return false;
+};
